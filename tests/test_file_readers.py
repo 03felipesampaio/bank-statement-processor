383be0fe1a4552371ff8805df50173cdefc6e68e
Felipe Sampaio <03.felipesampaio@gmail.com>
2023-09-12 22:14:08 -0300

    Update opening file method

--- tests/test_file_readers.py
+++ tests/test_file_readers.py
@@ -1,4 +1,5 @@
 import pytest
+from pathlib import Path
 from decimal import Decimal, getcontext
 from src.statement_readers.inter_bank_statement_reader import InterBankStatementFileReader, convert_brazilian_real_notation_to_decimal
 
@@ -6,7 +7,7 @@ getcontext().prec = 2
 
 
 class TestInterBankFileReader:
-    dummy_file_path = "tests/mocks/inter_statement_file_mock.csv"
+    dummy_file_path = Path("tests/mocks/inter_statement_file_mock.csv")
 
     @pytest.mark.parametrize('value,expected', [('-1,25', Decimal('-1.25')), ('-45.123.321,09', Decimal('-45123321.09')), ('2.345,45', Decimal('2345.45')), ('0,00', Decimal('0.00'))])
     def test_convert_brazilian_real_to_decimal_with_valid_values(self, value, expected):
@@ -14,7 +15,8 @@ class TestInterBankFileReader:
         assert converted_value == expected
 
     def test_read_file_with_valid_transactions(self):
-        statement = InterBankStatementFileReader(self.dummy_file_path)
+        with open(self.dummy_file_path) as csv_file:
+            statement = InterBankStatementFileReader(self.dummy_file_path.name, csv_file)
 
         assert len(statement.transactions) == 6
         assert statement.account_number == '123456789'
