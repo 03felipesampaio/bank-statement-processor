e7730476e04358f6d0dfdd9a7a1e4c01ad006df0
Felipe Sampaio <03.felipesampaio@gmail.com>
2023-09-15 01:54:07 -0300

    Add bank crud

--- src/crud.py
+++ src/crud.py
@@ -1,4 +1,21 @@
+from sqlalchemy.orm import Session
+from . import models
 from .statement_readers import InterBankStatementFileReader, NubankBankStatementFileReader
+
+
+def add_bank(db: Session, bank_name: str):
+    new_bank = models.Bank(name=bank_name)
+    db.add(new_bank)
+    db.commit()
+
+    return new_bank
+
+
+def get_bank(db: Session, bank_name: str):
+    return db.get(models.Bank, bank_name)
+
+
+# def add_transaction(db: Session, transaction)
 
 
 def insert_inter_bank_transactions_from_file(file_name: str, file):
