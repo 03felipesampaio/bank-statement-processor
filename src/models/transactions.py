e1c81c240d22dcecc38b8a8e22f450c928d925fa
Felipe Sampaio <03.felipesampaio@gmail.com>
2023-09-15 02:29:29 -0300

    Add transaction first model

---
+++ src/models/transactions.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.types import DECIMAL
+from ..database import Base
+
+
+class Transaction (Base):
+    __tablename__ = "transactions"
+
+    id = Column(String(64), primary_key=True)
+    bank = Column(String(50), nullable=False)
+    type = Column(String(50), nullable=False)
+    description = Column(String(200), nullable=False)
+    value = Column(DECIMAL(15, 2), nullable=False)
+
