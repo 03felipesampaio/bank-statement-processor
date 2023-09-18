2de2ce2f7ab850b7a292aecf4ca45fd609400a84
Felipe Sampaio <03.felipesampaio@gmail.com>
2023-09-18 19:36:33 -0300

    Add date column to transaction model

--- src/models/transactions.py
+++ src/models/transactions.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.types import DECIMAL
+from sqlalchemy.types import DECIMAL, Date
+from sqlalchemy.orm import relationship
 from ..database import Base
 
 
@@ -7,8 +8,8 @@ class Transaction (Base):
     __tablename__ = "transactions"
 
     id = Column(String(64), primary_key=True)
+    date = Column(Date, index=True, nullable=False)
     bank = Column(String(50), nullable=False)
     type = Column(String(50), nullable=False)
     description = Column(String(200), nullable=False)
     value = Column(DECIMAL(15, 2), nullable=False)
-
