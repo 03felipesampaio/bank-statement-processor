f2d43dd1eb5caabc32ecfeb4c61946594feeeeff
Felipe Sampaio <03.felipesampaio@gmail.com>
2024-04-03 11:13:12 -0300

    Add Credit Card Bill model

--- src/dto.py
+++ src/dto.py
@@ -1,7 +1,16 @@
 from pydantic import BaseModel
+from datetime import date
 
 
 class Transaction (BaseModel):
-    date: str
+    date: date
     description: str
+    category: str|None
     value: float
+    
+
+class CreditCardBill (BaseModel):
+    bill_date: date
+    value: float
+    
+    transactions: list[Transaction]
