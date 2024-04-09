ea5fe9c1a670ac9dd72add7b012a4b8557828ab4
Felipe Sampaio <03.felipesampaio@gmail.com>
2024-04-09 13:51:49 -0300

    Add transaction type

--- src/models/transaction.py
+++ src/models/transaction.py
@@ -1,8 +1,9 @@
 from datetime import date
 
 class Transaction:
-    def __init__(self, transaction_date: date, description: str, value: float, category: str|None = None) -> None:
+    def __init__(self, transaction_date: date, transaction_type: str, description: str, value: float, category: str|None = None) -> None:
         self.date = transaction_date
+        self.type = transaction_type
         self.description = description
         self.value = value
         self.category = category
--- src/readers/ofx_reader.py
+++ src/readers/ofx_reader.py
@@ -5,29 +5,24 @@ from ..models import BankStatement, Transaction
 class OFXReader:
     def read_transaction_from_ofx(self, ofx_transaction) -> Transaction:
         transaction = Transaction(
-            ofx_transaction.date,
+            ofx_transaction.date.date(),
+            ofx_transaction.type,
             ofx_transaction.memo,
             ofx_transaction.amount,
-            ofx_transaction.type
         )
-
-        print(ofx_transaction.date,)
 
         return transaction
 
     def read(self, file_content) -> BankStatement:
         ofx = OfxParser.parse(file_content)
-        # print(f'{dir(ofx)=}')
-        # print(f'{dir(ofx.accounts)=}')
-        # print(f'{dir(ofx.account)=}')
 
         account = ofx.account
         statement = account.statement
 
         bank_statement = BankStatement(
-            account.institution,
-            statement.start_date,
-            statement.end_date,
+            account.institution.organization,
+            statement.start_date.date(),
+            statement.end_date.date(),
             account.account_id,
             [self.read_transaction_from_ofx(tr) for tr in statement.transactions]
         )
