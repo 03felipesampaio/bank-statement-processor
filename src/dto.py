22da3b246609fac7b606332002d4afafaf5c513a
Felipe Sampaio <03.felipesampaio@gmail.com>
2024-04-09 13:51:17 -0300

    Add bank statement object

--- src/dto.py
+++ src/dto.py
@@ -4,22 +4,24 @@ from datetime import date
 
 class Transaction (BaseModel):
     date: date
+    type: str
     description: str
     category: str|None
     value: float
     
 
 class CreditCardBill (BaseModel):
+    bank_name: str
+    reference_month: str
     bill_date: date
-    value: float
-    
-    transactions: list[Transaction]
+    value : float
+    transactions : list[Transaction]
 
 
 class BankStatement (BaseModel):
     bank_name: str
     start_date: date
     end_date: date
-    account_id: str
+    # account_id: str
 
     transactions: list[Transaction]
