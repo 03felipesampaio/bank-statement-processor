08e3a33adcaea0c3f082b9db05a7a732ae5e9542
Felipe Sampaio <03.felipesampaio@gmail.com>
2024-08-12 13:05:20 -0300

    Add more functions to interface

--- src/readers/credit_card_pdf_reader.py
+++ src/readers/credit_card_pdf_reader.py
@@ -1,7 +1,20 @@
-import fitz
+from fitz import Document
+from datetime import date
 
 from .. import models
 
 class CreditCardPDFReader:
-    def read(self, document: fitz.Document) -> models.CreditCardBill:
-        raise NotImplementedError
+    def read(self, document: Document) -> models.CreditCardBill:
+        pass
+    
+    def read_bill_date(self, content: str) -> date:
+        pass
+    
+    def read_bill_value(self, content: str) -> float:
+        pass
+    
+    def read_period(self, bill_date: date) -> tuple[date, date]:
+        pass
+    
+    def read_transactions(self, content) -> list[models.Transaction]:
+        pass
