172c1d23fc65190be499a79ef2c32094fb440b8a
Felipe Sampaio <03.felipesampaio@gmail.com>
2024-10-21 23:45:36 -0300

    Add tests to parquet

--- tests/test_file_types.py
+++ tests/test_file_types.py
@@ -228,6 +228,118 @@ def test_write_bill_as_xlsx():
     assert df.iloc[1]["transaction_category"] == "Refund"
 
 
+def test_write_bill_as_parquet():
+    # Create a mock CreditCardBill object
+    transactions = [
+        Transaction(
+            transaction_date=datetime.date(2023, 1, 1),
+            transaction_type="purchase",
+            description="Electronics Store",
+            value=-200.0,
+            category="Electronics",
+        ),
+        Transaction(
+            transaction_date=datetime.date(2023, 1, 2),
+            transaction_type="refund",
+            description="Returned Item",
+            value=50.0,
+            category="Refund",
+        ),
+    ]
+    bill = CreditCardBill(
+        bank_name="Test Bank",
+        bill_date=datetime.date(2023, 1, 31),
+        value=150.0,
+        start_date=datetime.date(2023, 1, 1),
+        end_date=datetime.date(2023, 1, 31),
+        transactions=transactions,
+    )
+
+    # Write the bill to a Parquet file
+    file_content = file_types.write_bill_as("parquet", bill)
+
+    # Check the file content
+    assert isinstance(file_content, io.BytesIO)
+    file_content.seek(0)
+    df = pd.read_parquet(file_content)
+
+    assert df.shape == (2, 11)  # 2 transactions, 11 columns
+
+    assert df.iloc[0]["bank_name"] == "Test Bank"
+    assert df.iloc[0]["bill_date"] == datetime.date(2023, 1, 31)
+    assert df.iloc[0]["bill_reference_month"] == "2023-01"
+    assert df.iloc[0]["bill_value"] == 150.0
+    assert df.iloc[0]["bill_start_date"] == datetime.date(2023, 1, 1)
+    assert df.iloc[0]["bill_end_date"] == datetime.date(2023, 1, 31)
+    assert df.iloc[0]["transaction_date"] == datetime.date(2023, 1, 1)
+    assert df.iloc[0]["transaction_type"] == "purchase"
+    assert df.iloc[0]["transaction_description"] == "Electronics Store"
+    assert df.iloc[0]["transaction_value"] == -200.0
+    assert df.iloc[0]["transaction_category"] == "Electronics"
+
+    assert df.iloc[1]["bank_name"] == "Test Bank"
+    assert df.iloc[1]["transaction_date"] == datetime.date(2023, 1, 2)
+    assert df.iloc[1]["transaction_type"] == "refund"
+    assert df.iloc[1]["transaction_description"] == "Returned Item"
+    assert df.iloc[1]["transaction_value"] == 50.0
+    assert df.iloc[1]["transaction_category"] == "Refund"
+
+
+def test_write_statement_as_parquet():
+    # Create a mock BankStatement object
+    transactions = [
+        Transaction(
+            transaction_date=datetime.date(2023, 1, 1),
+            transaction_type="debit",
+            description="Grocery Store",
+            value=-50.0,
+            category="Groceries",
+        ),
+        Transaction(
+            transaction_date=datetime.date(2023, 1, 2),
+            transaction_type="credit",
+            description="Salary",
+            value=1500.0,
+            category="Income",
+        ),
+    ]
+    statement = BankStatement(
+        bank_name="Test Bank",
+        start_date=datetime.date(2023, 1, 1),
+        end_date=datetime.date(2023, 1, 31),
+        account_id="0123456789",
+        transactions=transactions,
+    )
+
+    # Write the statement to a Parquet file
+    file_content = file_types.write_statement_as("parquet", statement)
+
+    # Check the file content
+    assert isinstance(file_content, io.BytesIO)
+    file_content.seek(0)
+    df = pd.read_parquet(file_content)
+
+    assert df.shape == (2, 9)  # 2 transactions, 9 columns
+
+    assert df.iloc[0]["bank_name"] == "Test Bank"
+    print(type(df.iloc[0]["statement_start_date"]))
+    assert df.iloc[0]["statement_start_date"] == datetime.date(2023, 1, 1)
+    assert df.iloc[0]["statement_end_date"] == datetime.date(2023, 1, 31)
+    assert str(df.iloc[0]["statement_account_id"]) == "0123456789"
+    assert df.iloc[0]["transaction_date"] == datetime.date(2023, 1, 1)
+    assert df.iloc[0]["transaction_type"] == "debit"
+    assert df.iloc[0]["transaction_description"] == "Grocery Store"
+    assert df.iloc[0]["transaction_value"] == -50.0
+    assert df.iloc[0]["transaction_category"] == "Groceries"
+
+    assert df.iloc[1]["bank_name"] == "Test Bank"
+    assert df.iloc[1]["transaction_date"] == datetime.date(2023, 1, 2)
+    assert df.iloc[1]["transaction_type"] == "credit"
+    assert df.iloc[1]["transaction_description"] == "Salary"
+    assert df.iloc[1]["transaction_value"] == 1500.0
+    assert df.iloc[1]["transaction_category"] == "Income"
+
+
 def test_write_statement_as_csv():
     # Create a mock BankStatement object
     transactions = [
