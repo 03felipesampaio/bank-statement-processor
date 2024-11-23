68aa5b5f5b956ec624f5f3c2e2529b46fc5b4fa1
Felipe Amorim <03.felipesampaio@gmail.com>
2024-11-23 18:54:12 -0300

    Add equal function to Transaction

--- src/models/transaction.py
+++ src/models/transaction.py
@@ -1,9 +1,26 @@
 from datetime import date
 
+
 class Transaction:
-    def __init__(self, transaction_date: date, transaction_type: str|None, description: str, value: float, category: str|None = None) -> None:
+    def __init__(
+        self,
+        transaction_date: date,
+        transaction_type: str | None,
+        description: str,
+        value: float,
+        category: str | None = None,
+    ) -> None:
         self.date = transaction_date
         self.type = transaction_type
         self.description = description
         self.value = value
         self.category = category
+
+    def __eq__(self, value):
+        return (
+            self.date == value.date
+            and self.type == value.type
+            and self.description == value.description
+            and self.value == value.value
+            and self.category == value.category
+        )
