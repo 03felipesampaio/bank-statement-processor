c5be7d5716c592bf88bdfdc7ad89e7030ebaf998
Felipe Amorim <03.felipesampaio@gmail.com>
2024-11-23 01:18:15 -0300

    Add errors to decimal conversion

--- src/utils.py
+++ src/utils.py
@@ -10,4 +10,7 @@ def convert_brazilian_real_notation_to_decimal(brazilian_real_value: str) -> Dec
         '25.000,00' -> Decimal(25000.00)
     """
     value_without_point = re.sub(r'[^\d-]', '', brazilian_real_value)
-    return Decimal(value_without_point[:-2]+'.'+value_without_point[-2:])
+    try:
+        return Decimal(value_without_point[:-2]+'.'+value_without_point[-2:])
+    except Exception as e:
+        raise ValueError(f"Invalid brazilian real value: {brazilian_real_value}") from e
