de48695ace166a3f10e69e112492d20495b1fb88
Felipe Sampaio <03.felipesampaio@gmail.com>
2025-05-18 13:53:03 -0300

    Add file hash calculation for uploaded files in Nubank and Inter endpoints

--- src/dto.py
+++ src/dto.py
@@ -5,27 +5,45 @@ from decimal import Decimal
 
 
 class Transaction(BaseModel):
-    date: datetime.date = Field(..., description='The date of the transaction')
-    type: str | None = Field(..., description='The type of the transaction, e.g., debit or credit')
-    description: str = Field(..., description='A brief description of the transaction')
-    category: str | None = Field(..., description='The category of the transaction, e.g., groceries, utilities')
-    value: Decimal = Field(..., description='The monetary value of the transaction')
-    
+    date: datetime.date = Field(..., description="The date of the transaction")
+    type: str | None = Field(
+        ..., description="The type of the transaction, e.g., debit or credit"
+    )
+    description: str = Field(..., description="A brief description of the transaction")
+    category: str | None = Field(
+        ..., description="The category of the transaction, e.g., groceries, utilities"
+    )
+    value: Decimal = Field(..., description="The monetary value of the transaction")
+
 
 class CreditCardBill(BaseModel):
     model_config = ConfigDict(
-            alias_generator=AliasGenerator(
-                serialization_alias=to_camel,
-            )
+        alias_generator=AliasGenerator(
+            serialization_alias=to_camel,
         )
-    
-    bank_name: str = Field(..., description='The name of the bank issuing the credit card')
-    reference_month: str = Field(..., description='The reference month for the credit card bill, formatted as "YYYY-MM"')
-    bill_date: datetime.date = Field(..., description='The date the bill was due')
-    start_date: datetime.date = Field(..., description='The start date of the billing period')
-    end_date: datetime.date = Field(..., description='The end date of the billing period')
-    value: Decimal = Field(..., description='The total value of the credit card bill')
-    transactions: list[Transaction] = Field(..., description='A list of transactions included in the bill')
+    )
+
+    file_hash: str = Field(
+        ..., description="A unique hash of the file for verification purposes"
+    )
+    bank_name: str = Field(
+        ..., description="The name of the bank issuing the credit card"
+    )
+    reference_month: str = Field(
+        ...,
+        description='The reference month for the credit card bill, formatted as "YYYY-MM"',
+    )
+    bill_date: datetime.date = Field(..., description="The date the bill was due")
+    start_date: datetime.date = Field(
+        ..., description="The start date of the billing period"
+    )
+    end_date: datetime.date = Field(
+        ..., description="The end date of the billing period"
+    )
+    value: Decimal = Field(..., description="The total value of the credit card bill")
+    transactions: list[Transaction] = Field(
+        ..., description="A list of transactions included in the bill"
+    )
 
 
 class BankStatement(BaseModel):
@@ -35,9 +53,21 @@ class BankStatement(BaseModel):
         )
     )
     
-    bank_name: str = Field(..., description='The name of the bank issuing the statement')
-    start_date: datetime.date = Field(..., description='The start date of the statement period')
-    end_date: datetime.date = Field(..., description='The end date of the statement period')
+    file_hash: str = Field(
+        ..., description="A unique hash of the file for verification purposes"
+    )
+
+    bank_name: str = Field(
+        ..., description="The name of the bank issuing the statement"
+    )
+    start_date: datetime.date = Field(
+        ..., description="The start date of the statement period"
+    )
+    end_date: datetime.date = Field(
+        ..., description="The end date of the statement period"
+    )
     # account_id: str
 
-    transactions: list[Transaction] = Field(..., description='A list of transactions included in the statement')
+    transactions: list[Transaction] = Field(
+        ..., description="A list of transactions included in the statement"
+    )
--- src/models/credit_card_bill.py
+++ src/models/credit_card_bill.py
@@ -2,13 +2,14 @@ from . import Transaction
 from datetime import date
 
 class CreditCardBill:
-    def __init__(self, bank_name, bill_date: date, value: float, start_date: date, end_date: date, transactions: list[Transaction] = None) -> None:
+    def __init__(self, bank_name, bill_date: date, value: float, start_date: date, end_date: date, transactions: list[Transaction] = None, file_hash: str = None) -> None:
         self.bank_name = bank_name
         self.bill_date = bill_date
         self.reference_month = bill_date.strftime('%Y-%m')
         self.value = value
         self.start_date = start_date
         self.end_date = end_date
+        self.file_hash = file_hash
         
         if not transactions:
             self.transactions: list[Transaction] = []
